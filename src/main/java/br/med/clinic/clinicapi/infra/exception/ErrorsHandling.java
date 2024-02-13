bc32b418bf6647032e012d0a93305a547ee99e20
Diogo O Santos <diogogyn100@gmail.com>
2024-02-12 22:41:24 -0300

    tratando mais erros

--- src/main/java/br/med/clinic/clinicapi/infra/exception/ErrorsHandling.java
+++ src/main/java/br/med/clinic/clinicapi/infra/exception/ErrorsHandling.java
@@ -1,12 +1,16 @@
 package br.med.clinic.clinicapi.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.nio.file.AccessDeniedException;
 import java.util.List;
 
 @RestControllerAdvice
@@ -23,10 +27,32 @@ public class ErrorsHandling {
 
         return ResponseEntity.badRequest().body(fieldErrors.stream().map(ErrorValidationRecord::new).toList());
     }
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity handlingErrorBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity handlingErrorAuthentication() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falha na autenticação");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity handlingErrorAcessoNegado() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity handlingError500(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " +ex.getLocalizedMessage());
+    }
+
 
     private record ErrorValidationRecord(String field, String message){
         public ErrorValidationRecord(FieldError erro){
             this(erro.getField(), erro.getDefaultMessage());
         }
     }
+
+
 }
