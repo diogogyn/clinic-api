710e1e9699a3c6551149ee1af995497a0c4dfe85
Diogo O Santos <diogogyn100@gmail.com>
2024-03-04 13:19:44 -0300

    add service admin

--- src/main/java/br/med/clinic/clinicapi/infra/administration/service/AdministrationService.java
+++ src/main/java/br/med/clinic/clinicapi/infra/administration/service/AdministrationService.java
@@ -1,15 +1,17 @@
 package br.med.clinic.clinicapi.infra.administration.service;
 
 import br.med.clinic.clinicapi.domain.user.repository.UserRepository;
+import br.med.clinic.clinicapi.infra.administration.repository.ProfileRepository;
+import br.med.clinic.clinicapi.infra.administration.repository.UserProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdministrationService {
-//    @Autowired
-//    private UserProfileRepository userProfileRepository;
-//    @Autowired
-//    private ProfileRepository profileRepository;
+    @Autowired
+    private UserProfileRepository userProfileRepository;
+    @Autowired
+    private ProfileRepository profileRepository;
     @Autowired
     private UserRepository userRepository;
 
